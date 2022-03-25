d0fd9a703e9db4cd7295dc31d9a46a684a57a4a1
LSaldyt <lucassaldyt@gmail.com>
2022-03-25 14:25:31 -0700

    Add basic plotting

--- main.py
+++ main.py
@@ -12,19 +12,29 @@ from solutions.simulated_annealing import simulated_annealing
 # This is someone elses dynamic programming solution, for comparison
 from python_tsp.exact import solve_tsp_dynamic_programming
 
+def dynamic_programming(problem, settings):
+    return solve_tsp_dynamic_programming(problem.distance_matrix)[0]
+
 def main():
-    settings = Settings(rng=np.random.default_rng(2022), alpha=0.99, k=100)
-    for i in range(1, 6):
-        n_cities = 2**i
-        problem  = Problem(n_cities=n_cities, size=10)
-        start    = time()
-        approx   = simulated_annealing(problem, settings)
-        mid      = time()
-        exact, _ = solve_tsp_dynamic_programming(problem.distance_matrix)
-        end      = time()
-        print(n_cities)
-        print('Approximate:', problem.cost(approx), mid - start)
-        print('Exact:      ', problem.cost(exact),  end - mid)
+    settings = Settings(rng=np.random.default_rng(2022), alpha=0.99, k=1000, repeats=10)
+    algs = [('Simulated Annealing', simulated_annealing),
+            ('Dynamic Programming', dynamic_programming)]
+    i_max = 8; j = 0
+    stats = np.zeros(((i_max - 1) * len(algs) * settings.repeats, 4))
+    for i in range(1, i_max):
+        n_cities = 2*i
+        for n in range(settings.repeats):
+            problem  = Problem(n_cities=n_cities, size=10)
+            for name, algorithm in algs:
+                start    = time()
+                solution = algorithm(problem, settings)
+                end      = time()
+                cost     = problem.cost(solution)
+                duration = end - start
+                stats[j, :] = (n_cities, cost, duration, 1 if 'Dynamic' in name else 0)
+                j += 1
+                print(f'{name}: {cost:4.6f} in {duration:4.6f} {n_cities:3} cities, repeat {n:3}')
+    np.savez('data.npz', stats=stats)
 
 if __name__ == '__main__':
     main()
--- plot.py
+++ plot.py
@@ -1,1 +1,45 @@
+import plotly.graph_objects as go
 import plotly
+import numpy  as np
+import pandas as pd
+
+from subprocess import call
+
+QUAL_COLORS = plotly.colors.qualitative.G10 + plotly.colors.qualitative.Dark24 + plotly.colors.qualitative.Light24
+
+def save(fig, name, w=1000, h=600, parentdir='data'):
+    fig.show()
+    fig.write_image(f'{parentdir}/{name}.svg', width=w, height=h)
+    fig.write_image(f'{parentdir}/{name}.png', width=w, height=h)
+    call(f'rsvg-convert -f pdf -o {parentdir}/{name}.pdf {parentdir}/{name}.svg',
+         shell=True)
+
+def main():
+    stats = np.load('data.npz')['stats']
+    algs  = ('Simulated Annealing', 'Dynamic Programming')
+    df    = pd.DataFrame(data=stats,
+                         columns=['n_cities', 'cost', 'duration', 'algorithm'])
+    fig = go.Figure()
+    for i, alg in enumerate(algs):
+        sub = df[df['algorithm'] == i]
+        fig.add_trace(go.Scatter(x=sub['cost'], y=sub['duration'],
+                                 marker=dict(color=QUAL_COLORS[i]),
+                                 mode='markers',
+                                 name=alg))
+    fig.update_layout(
+        font=dict(family='Times', size=14, color='black'),
+        xaxis_title='Cost',
+        yaxis_title='Duration',
+        legend=dict(
+            orientation="h",
+            yanchor='top',
+            y=1.0,
+            xanchor='center',
+            x=0.5,
+            bgcolor='white',
+            bordercolor='black',
+            borderwidth=1),)
+    save(fig, 'comparison', w=1920, h=1080)
+
+if __name__ == '__main__':
+    main()
